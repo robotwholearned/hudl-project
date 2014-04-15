7b2940708a51069516dbfe74dc423704f6abd392
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-04-15 09:44:24 -0600

    create project

---
+++ hudl/hudl/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hudl
+//
+//  Created by Cassandra Sandquist on 2014-04-15.
+//  Copyright (c) 2014 self.edu.robotwholearned. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hudl/hudl/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  hudl
+//
+//  Created by Cassandra Sandquist on 2014-04-15.
+//  Copyright (c) 2014 self.edu.robotwholearned. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
